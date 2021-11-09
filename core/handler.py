e23431eed5ac1cabe160d2b189b54c321459d6b2
Jermaine <jermainenee@yeah.net>
2021-11-09 16:29:34 +0800

    添加post动态入参

--- core/handler.py
+++ core/handler.py
@@ -1,4 +1,6 @@
 import operator
+import time
+import json
 
 import akshare as ak
 from django.http import HttpResponse, JsonResponse
@@ -6,10 +8,12 @@ from pandas import DataFrame
 
 
 def core_handle(request):
-    print(request)
+    post = request.POST.dict()
+    print(time.strftime("%H:%M:%S", time.localtime())+"-路径[%s]"%(request.path)+"-参数[%s]"%(post))
+    print(post)
     if request.method == 'POST':
-        method = getattr(ak, request.path_info.replace('/api/', ''))
-        return HttpResponse(content=DataFrame.to_json(method(),orient='records'), content_type='application/json')
+        method = getattr(ak, request.path_info.replace('/api/', ''))(post.keys(), post.values())
+        return HttpResponse(content=DataFrame.to_json(method(), orient='records'), content_type='application/json')
 
     elif request.method == 'GET':
         return HttpResponse(JsonResponse({'state': False, 'reason': 'request is invalid'}))
