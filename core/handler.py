8cadfc45175eff11347fbdc52ad0931279d9ae57
Jermaine <jermainenee@yeah.net>
2021-11-09 17:24:27 +0800

    添加post动态入参

--- core/handler.py
+++ core/handler.py
@@ -9,11 +9,16 @@ from pandas import DataFrame
 
 def core_handle(request):
     post = request.POST.dict()
+    valuse = post.values()
+    keys = post.keys()
     print(time.strftime("%H:%M:%S", time.localtime())+"-路径[%s]"%(request.path)+"-参数[%s]"%(post))
     print(post)
     if request.method == 'POST':
-        method = getattr(ak, request.path_info.replace('/api/', ''))(post.values(), post.keys())
-        return HttpResponse(content=DataFrame.to_json(method(), orient='records'), content_type='application/json')
+        api_func = getattr(ak, request.path_info.replace('/api/', ''))
+        print(api_func.__name__)
+        result = api_func(**post)
+        print(result)
+        # return HttpResponse(content=DataFrame.to_json(result, orient='records'), content_type='application/json')
 
     elif request.method == 'GET':
         return HttpResponse(JsonResponse({'state': False, 'reason': 'request is invalid'}))
