6c8bb076c3033741149ee857d6be96625fb8422c
nzm <zimingnee@163.com>
2021-11-09 22:07:02 +0800

    ces

--- core/handler.py
+++ core/handler.py
@@ -12,13 +12,9 @@ def core_handle(request):
     valuse = post.values()
     keys = post.keys()
     print(time.strftime("%H:%M:%S", time.localtime())+"-路径[%s]"%(request.path)+"-参数[%s]"%(post))
-    print(post)
     if request.method == 'POST':
-        api_func = getattr(ak, request.path_info.replace('/api/', ''))
-        print(api_func.__name__)
-        result = api_func(**post)
-        print(result)
-        # return HttpResponse(content=DataFrame.to_json(result, orient='records'), content_type='application/json')
+        method = getattr(ak, request.path_info.replace('/api/', ''))
+        return HttpResponse(content=DataFrame.to_json(method(),orient='records'), content_type='application/json')
 
     elif request.method == 'GET':
         return HttpResponse(JsonResponse({'state': False, 'reason': 'request is invalid'}))
